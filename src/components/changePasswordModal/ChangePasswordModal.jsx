6cfcae3a68a73142abdfe7d5cbbf38cb16fa1967
Orobosa <95650588+orobosaik@users.noreply.github.com>
2023-11-21 02:44:31 +0100

    Update: Change page structure of change password modal Add styling to change password modal

--- src/components/changePasswordModal/ChangePasswordModal.jsx
+++ src/components/changePasswordModal/ChangePasswordModal.jsx
@@ -1,16 +1,12 @@
 import "./changePasswordModal.css";
 import {
-	AddPhotoAlternateRounded,
-	FileUploadRounded,
-	Image,
 	LockOutlined,
 	MailOutlineRounded,
-	UploadFileRounded,
 	VisibilityOffOutlined,
 	VisibilityOutlined,
 } from "@mui/icons-material";
 import ToggleSwitch from "../toggleSwitch/ToggleSwitch";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { CloseRounded, EditRounded } from "@mui/icons-material";
 import { CircularProgress } from "@mui/material";
 import { ToastContainer } from "react-toastify";
@@ -19,6 +15,10 @@ export default function ChangePasswordModal({ ...props }) {
 	const [open, setOpen] = useState(false);
 	const [showPassword, setShowPassword] = useState(false);
 	const [loading, setLoading] = useState(false);
+
+			const email = useRef();
+			const password = useRef();
+
 
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
@@ -49,25 +49,14 @@ export default function ChangePasswordModal({ ...props }) {
 	}
 
 	useEffect(() => {
-		const modal = document.querySelector(".adminOfficeEditModal");
+		const modal = document.querySelector(".changePasswordModal");
 		if (modal) {
 			open ? modal.showModal() : modal.close();
 		}
 		return () => {};
 	}, [open]);
 
-	const [isActive, setIsActive] = useState(true);
-	const [isManagement, setIsManagement] = useState(false);
 
-	const [photo, setPhoto] = useState(null);
-	const [clearPhotoButton, setClearPhotoButton] = useState(false);
-
-	const onPhotoChange = (event) => {
-		if (event.target.files && event.target.files[0]) {
-			setPhoto(URL.createObjectURL(event.target.files[0]));
-			console.log(URL.createObjectURL(event.target.files[0]));
-		}
-	};
 
 	return (
 		<>
@@ -83,11 +72,11 @@ export default function ChangePasswordModal({ ...props }) {
 						<div className="modalViewCloseButton" onClick={handleClose}>
 							<CloseRounded className="closeButton" />
 						</div>
+							{/* <img src="Mpphurd.png" alt="Mpphurd Logo" /> */}
 					</header>
 
-					<form action="#" className=" login__form" onSubmit={handleSubmit}>
+					 <form action="#" className=" login__form" onSubmit={handleSubmit}>
 						<div className="form-logo">
-							{/* <img src="Mpphurd.png" alt="Mpphurd Logo" /> */}
 						</div>
 						<p className="form-greeting"> Please Login to continue</p>
 						<div className="form-input">
@@ -131,10 +120,39 @@ export default function ChangePasswordModal({ ...props }) {
 								</span>
 							</div>
 						</div>
+						<div className="form-input">
+							<label htmlFor="password">Confirm Password</label>
+							<div>
+								<span>
+									<LockOutlined className="formIcon" />
+								</span>
+								<input
+									type={showPassword ? "text" : "password"}
+									name="password2"
+									id="password2"
+									placeholder="Confirm your new password"
+									required
+									ref={password}
+								/>
+								<span
+									className="btn"
+									onClick={() => setShowPassword(!showPassword)}>
+									{showPassword ? (
+										<VisibilityOutlined className="formIcon" />
+									) : (
+										<VisibilityOffOutlined className="formIcon" />
+									)}
+								</span>
+							</div>
+						</div>
 
+						<ToastContainer />
+
+						<footer>
 						<button
 							type="submit"
-							className="btn btn-form-submit"
+							className="btn primary"
+							// className="btn btn-form-submit"
 							disabled={loading}>
 							{loading ? (
 								<CircularProgress
@@ -143,93 +161,11 @@ export default function ChangePasswordModal({ ...props }) {
 									sx={{ color: "white" }}
 								/>
 							) : (
-								"Login"
+								"Continue"
 							)}
 						</button>
-						<ToastContainer />
-
-						<footer>
-							<button className="primary">Save</button>
 						</footer>
 					</form>
-
-					{/*
-					<div className="applicationForm">
-						<div className="applicationInputWrapper">
-							<div className="applicationItemsWrapper">
-								<div className="applicationTitle">
-									<h3>Staff Personal Information</h3>
-								</div>
-								<div className="applicationItems">
-									<div className="applicationItem">
-										<label htmlFor={"officeName"}>Office Name:</label>
-										<input type="text" name={"officeName"} id={"officeName"} />
-									</div>
-
-									<div className="applicationItem">
-										<label htmlFor={"officeName"}>Officer In Charge:</label>
-										<input type="text" name={"officeName"} id={"officeName"} />
-									</div>
-
-									<div>
-										<label htmlFor="staffOffice">Position:</label>
-										<select name="staffOffice" id="staffOffice">
-											<option value="proposed">Proposed</option>
-											<option value="underConstruction">
-												Under Construction
-											</option>
-											<option value="built">Built</option>
-										</select>
-									</div>
-
-									<div className="applicationItem">
-										<label htmlFor="">Office and Tasks</label>
-										<div className="inputStaffOfficeWrapper">
-											<div className="inputStaffOfficeList">
-												<div className="inputStaffOffice">
-													<button>-</button>
-													<div>
-														<label>Office</label>
-														<select name="" id=""></select>
-													</div>
-													<div>
-														<label>Tasks</label>
-														<div className="taskList">
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Task 1</label>
-															</span>
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Minute FIle</label>
-															</span>
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Create Plan</label>
-															</span>
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Send File</label>
-															</span>
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Upload Document</label>
-															</span>
-															<span>
-																<input type="checkbox" name="task" id="task" />
-																<label htmlFor="task">Send Message</label>
-															</span>
-														</div>
-													</div>
-												</div>
-											</div>
-											<button>+</button>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div> */}
 
 				</dialog>
 			)}
