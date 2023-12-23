838acf90a70302a47006a2048b17666dd63c18ae
Orobosa <95650588+orobosaik@users.noreply.github.com>
2023-12-23 20:03:39 +0100

    Remove cookie on logout

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -20,10 +20,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { persistor } from "../../redux/store";
 import { getThemeColor } from "../../utilities/themeColor";
+import { useCookies } from "react-cookie";
 
 export default function SideBar({ selected }) {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
+	const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
 
 	const { theme } = useSelector((state) => state.app);
 	const themeColor = getThemeColor(theme);
@@ -31,6 +33,7 @@ export default function SideBar({ selected }) {
 	const handleLogout = () => {
 		console.log("YAYAYAYAYAYA");
 		dispatch(logout());
+		removeCookie("access_token")
 		navigate("/login");
 		// persistor.purge();
 
