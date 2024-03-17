7ce3fad79f058debc3a25a5057f42cee1a7cd48b
Orobosa <95650588+orobosaik@users.noreply.github.com>
2024-03-17 20:53:08 +0100

    Update routing logic handling staff and admin view to include use state

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,24 @@ import MainRoutes from "./utilities/MainRoutes";
 
 function App() {
 	// const [view, setView] = useState(null);
+	const [user, setUser] = useState(import.meta.env.VITE_USER_TYPE);
 
-	// useEffect(() => {
-	// 	const host = window.location.host;
+	useEffect(() => {
+		// 	const host = window.location.host;
+		// 	// const path = window.location.pathname;
+		// 	// const arr = path.split("/");
+		// 	// const arr = host.split(".").slice(0, host.includes("localhost") ? -1 : 1);
+		// 	const arr = host.split(".");
+		// 	if (arr[0].toLowerCase() === "admin-mpphurd") {
+		// 		setView("admin");
+		// 	} else if (arr[0].toLowerCase() === "mpphurd") {
+		// 		setView("staff");
+		// 	}
+		// 	// }
+		setUser(import.meta.env.VITE_USER_TYPE);
+	}, [window.location]);
 
-	// 	// const path = window.location.pathname;
-	// 	// const arr = path.split("/");
-
-	// 	// const arr = host.split(".").slice(0, host.includes("localhost") ? -1 : 1);
-	// 	const arr = host.split(".");
-
-	// 	if (arr[0].toLowerCase() === "admin-mpphurd") {
-	// 		setView("admin");
-	// 	} else if (arr[0].toLowerCase() === "mpphurd") {
-	// 		setView("staff");
-	// 	}
-
-	// 	// }
-	// }, [window.location.host]);
-
-	const user = import.meta.env.VITE_USER_TYPE;
+	// const user = import.meta.env.VITE_USER_TYPE;
 
 	return (
 		<>
